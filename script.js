e8f0e54c3a86583cb3829239c8a3eedb83118c99
Christian John Hurry <christianhurry@christians-mbp.home>
2026-02-01 10:38:33 +0000

    add initial cells to grid

--- script.js
+++ script.js
@@ -17,6 +17,19 @@ const cellSize = 20;
 const cols = Math.floor(canvas.width / cellSize);
 const rows = Math.floor(canvas.height / cellSize);
 
+const prob_init_alive = 0.1;
+
+// create grid as array of arrays 
+// each cell is alive (1) or dead (0)
+let grid = [];
+for (let y = 0; y < rows; y++) {
+    grid[y] = [];
+    for (let x = 0; x < cols; x++) {
+        //initialise cells
+        grid[y][x] = Math.random() < prob_init_alive ? 1 : 0; // shorthand if else 
+    }
+}
+
 
 // draw grid 
 function drawGrid() {
@@ -41,3 +54,17 @@ function drawGrid() {
 }
 
 drawGrid();
+
+
+function drawCells() {
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++){
+            if (grid[y][x] === 1) {
+                ctx.fillStyle = '#6eb5ff';
+                ctx.fillRect(x * cellSize, y*cellSize, cellSize, cellSize) // (x,y,width,height)
+            }
+        }
+    }
+}
+
+drawCells();
